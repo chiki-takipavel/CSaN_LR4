4864b0465be83442ca60c224694e5529b32805bb
Pavel Miskevich <chiki-takipavel@mail.ru>
2020-05-07 15:35:37 +0300

    Добавьте файлы проекта.

---
+++ LR4_CSaN/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LR4_CSaN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
